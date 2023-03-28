c7e8abc91e85e3a5aca8d32d6db53007d188ee1e
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-03-28 12:20:53 +0300

    add search

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,48 +1,97 @@
-import { Component } from 'react';
+import { useState } from "react";
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 
-export class Searchbar extends Component {
-  state = { inputValue: '' };
+export const Searchbar = ({onSubmit}) => {
+  const [ inputValue, setInputValue ] = useState('');
 
-  handleChange = event => {
-    this.setState({ inputValue: event.target.value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    if (this.state.inputValue.trim() === '') {
-      Notify.warning('Opss...try again');
-      return;
-    }
-    this.props.onSubmit(this.state.inputValue);
-    this.setState({ inputValue: '' });
-  };
-
-  render() {
-    return (
-      <header className={css.header}>
-        <form className={css.form} onSubmit={this.handleSubmit}>
-          <button className={css.buttonForm} type="submit">
-            <span>Search</span>
-          </button>
-
-          <input
-            className={css.SearchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images"
-            value={this.state.inputValue}
-            onChange={this.handleChange}
-          />
-        </form>
-      </header>
-    );
+  const handleChange = event => {
+    setInputValue(event.target.value);
   }
+
+  const handleSubmit = event => {
+      event.preventDefault();
+      if (inputValue.trim() === '') {
+        Notify.warning('Opss...try again');
+        return;
+      }
+      onSubmit(inputValue);
+      setInputValue('');
+  }
+
+  return (
+          <header className={css.header}>
+            <form className={css.form} onSubmit={handleSubmit}>
+              <button className={css.buttonForm} type="submit">
+                <span>Search</span>
+              </button>
+    
+              <input
+                className={css.SearchFormInput}
+                type="text"
+                autoComplete="off"
+                autoFocus
+                placeholder="Search images"
+                value={inputValue}
+                onChange={handleChange}
+              />
+            </form>
+          </header>
+        );
 }
+
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+
+
+
+
+
+// export class Searchbar extends Component {
+//   state = { inputValue: '' };
+
+//   handleChange = event => {
+//     this.setState({ inputValue: event.target.value });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.inputValue.trim() === '') {
+//       Notify.warning('Opss...try again');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.inputValue);
+//     this.setState({ inputValue: '' });
+//   };
+
+//   render() {
+//     return (
+//       <header className={css.header}>
+//         <form className={css.form} onSubmit={this.handleSubmit}>
+//           <button className={css.buttonForm} type="submit">
+//             <span>Search</span>
+//           </button>
+
+//           <input
+//             className={css.SearchFormInput}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images"
+//             value={this.state.inputValue}
+//             onChange={this.handleChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
